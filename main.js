00cad394f2ec3730de9ee34138b9dda76a5e3cf4
BTH-M3ND3S <129889620+BTH-M3ND3S@users.noreply.github.com>
2023-04-05 11:56:37 -0300

    alteração js

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ function botaosoma()
 {
     let caixa1 = document.getElementById( "num1" ).value; 
     let caixa2 = document.getElementById( "num2" ).value;
-    document.getElementById( "resultado" ).innerHTML = " = " + caixa1 + caixa2;
+    document.getElementById( "resultado" ).innerHTML = " = " + parseFloat(caixa1) + parsetFloat(caixa2);
     document.getElementById( "num1" ).value = "";
     document.getElementById( "num2" ).value = "";
     
