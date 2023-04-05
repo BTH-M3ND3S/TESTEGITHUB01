66db45fc3a8b06357b75f63b230a939a8fa2aa2f
BTH-M3ND3S <129889620+BTH-M3ND3S@users.noreply.github.com>
2023-04-05 12:25:03 -0300

    alteração js

--- main.js
+++ main.js
@@ -4,9 +4,15 @@ document.getElementById( "botao" ).addEventListener( "click" , exibenome );
 
 document.getElementById( "btn-soma" ).addEventListener( "click" , botaosoma );
 
+document.getElementById( "botao-dividir" ).addEventListener( "click" , botaodividir );
+
+document.getElementById( "botao-multiplicar" ).addEventListener( "click" , botaomultiplicar );
+
+document.getElementById( "botao-subtrair" ).addEventListener( "click" , botaosubtrair );
+
 function showcolor()
 {
-    document.body.style.backgroundColor = "red";
+    document.body.style.backgroundColor = "aqua";
 }
 
 function exibenome()
@@ -22,8 +28,34 @@ function botaosoma()
     let caixa3 = parseFloat(caixa1) + parseFloat(caixa2);
     document.getElementById( "resultado" ).innerHTML = " = " + caixa3;
     document.getElementById( "num1" ).value = "";
-    document.getElementById( "num2" ).value = "";
-    
+    document.getElementById( "num2" ).value = "";   
+}
+function botaodividir()
+{
+    let caixa4 = document.getElementById( "num3" ).value; 
+    let caixa5 = document.getElementById( "num4" ).value;
+    let caixa6 = parseFloat(caixa1) / parseFloat(caixa2);
+    document.getElementById( "resultado1" ).innerHTML = " = " + caixa6;
+    document.getElementById( "num3" ).value = "";
+    document.getElementById( "num4" ).value = "";
+}
+function botaomultiplicar()
+{
+    let caixa7 = document.getElementById( "num5" ).value; 
+    let caixa8 = document.getElementById( "num6" ).value;
+    let caixa9 = parseFloat(caixa1) * parseFloat(caixa2);
+    document.getElementById( "resultado2" ).innerHTML = " = " + caixa9;
+    document.getElementById( "num5" ).value = "";
+    document.getElementById( "num6" ).value = "";
+}
+function botaosubtrair()
+{
+    let caixa10 = document.getElementById( "num7" ).value; 
+    let caixa11 = document.getElementById( "num8" ).value;
+    let caixa12 = parseFloat(caixa1) / parseFloat(caixa2);
+    document.getElementById( "resultado3" ).innerHTML = " = " + caixa12;
+    document.getElementById( "num7" ).value = "";
+    document.getElementById( "num8" ).value = "";
 }
 
 
