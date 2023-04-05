b67a41ed732d80ed6fa7f1359c6c00da881b14a1
BTH-M3ND3S <129889620+BTH-M3ND3S@users.noreply.github.com>
2023-04-05 11:51:58 -0300

    alteração javascrpit

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ document.addEventListener ("click" , showcolor );
 
 document.getElementById( "botao" ).addEventListener( "click" , exibenome );
 
+document.getElementById( "btn-soma" ).addEventListener( "click" , botaosoma );
+
 function showcolor()
 {
     document.body.style.backgroundColor = "red";
@@ -12,6 +14,17 @@ function exibenome()
     let usuário = document.getElementById( "nome" ).value;
     document.getElementById( "mensagem" ).innerHTML = "Meu nome é " + usuário;
     document.getElementById( "nome" ).value = "";
-
 }
+function botaosoma()
+{
+    let usuário = document.getElementById( "num1" ).value; document.getElementById( "num2" ).value;
+    document.getElementById( "resultado" ).innerHTML = " = " + usuário;
+    document.getElementById( "num1" ).value = "";
+    document.getElementById( "num2" ).value = "";
+    
+}
+
+
+
+
 
